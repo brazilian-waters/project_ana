7533a9a5f94000fd81beb3fb3da34166c5baa70c
Nelson Matos <2swim.nelson@gmail.com>
2020-05-23 15:50:41 -0300

    Add reservoir_wrangler.py

---
+++ reservoir_wrangler.py
@@ -0,0 +1,18 @@
+""" """
+
+from config import DATA_DIR, SYSTEMS_URL
+
+def get_reservoir_code_name(url):
+    """Get the names and codes of reservoirs, of a given system, which is 
+    available on a given URL.
+    
+    Args:
+    url: string. The URL containing reservoir data of a given system.
+    
+    Returns:
+    res_table: Pandas DataFrame holding reservoirs codes and names.  
+    """
+    return None
+
+if __name__ == '__main__':
+    assembly_reservoir_table()
