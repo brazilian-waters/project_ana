7c381821a0445e2c59fb165f1341c4da92c51677
Nelson Matos <2swim.nelson@gmail.com>
2020-05-23 22:48:09 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
 """This module stores all constants used by the project"""
 
-# Folder to store all data.
-DATA_DIR = "data"
-
-SYSTEMS_URL = {'sin': r"https://www.ana.gov.br/sar0/MedicaoSin",
-               'nordestes_semiarido': r"https://www.ana.gov.br/sar0/Medicao",
-               'cantareira': r"https://www.ana.gov.br/sar0/MedicaoCantareira"}
-# URLs of each system. ANA's website shows reservoir data divided into 3 main 
-# systems: 'SIN', 'Sistema Cantareira' and 'Nordeste e Semiarido'.
