a163c5e208dd3626af7281200656720ec99b0941
Nelson Matos <2swim.nelson@gmail.com>
2020-06-02 21:26:02 -0300

    Fix missing data when the page isn't available All pages, of all reservoirs, are now saved as .html files in the disk, so you will not loose data when the page is not available.

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 """This module stores all constants used by the project"""
 
+import os
+
 DATA_DIR = "data" # Folder to store all data.
 
+RESERVOIR_DIR = os.path.join(DATA_DIR, "reservoirs")
--- reservoir_wrangler.py
+++ reservoir_wrangler.py
@@ -8,21 +8,22 @@ Data is grouped on three different systems, as follows: 'SIN',
 
 import datetime
 import os
-from collections import namedtuple
+import concurrent.futures
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from config import DATA_DIR
-
+from config import DATA_DIR, RESERVOIR_DIR
 
 ##############################################################################
+
+DATE_COL = "date"
 
 # Variables X text to scrape "Nordeste e Semiárido" system:
 NORDESTE = [("capacity_m3", "text-center coluna_4"), # Capacidade (hm³)
             ("level_m", "text-center coluna_3"), # Cota (m)
             ("vol_hm3", "text-center coluna_5"), # Volume (hm³)
             ("vol_100", "text-center coluna_6"), # Volume (%)
-            ("date", "text-center coluna_7")] # Data da Medição
+            (DATE_COL, "text-center coluna_7")] # Data da Medição
 
 # Variables X text to scrape "SIN - Sistema Interligado Nacional" system:
 SIN = [("level_m", "text-center coluna_3"), # Cota (m)
@@ -34,7 +35,7 @@ SIN = [("level_m", "text-center coluna_3"), # Cota (m)
        ("natural_flow_m3s", "text-center coluna_9"), # Volume Útil (%)
        ("incremental_flow_m3s", "text-center coluna_11"),
        # Vazão Incremental (m³/s)
-       ("date", "text-center coluna_13")] # Data da Medição
+       (DATE_COL, "text-center coluna_13")] # Data da Medição
 
 # Variables X text to scrape "Cantareira" system:
 CANTAREIRA = ([("level_m", "text-center coluna_3"), # Cota (m)
@@ -42,7 +43,7 @@ CANTAREIRA = ([("level_m", "text-center coluna_3"), # Cota (m)
                ("net_vol_100", "text-center coluna_5"), # Volume Útil (%)
                ("inflow_m3s", "text-center coluna_6"), # Afluência (m³/s)
                ("outflow_m3s", "text-center coluna_7"), # Defluência (m³/s)
-               ("date", "text-center coluna_8")]) # Data da Medição
+               (DATE_COL, "text-center coluna_8")]) # Data da Medição
 
 ##############################################################################
 
@@ -53,145 +54,152 @@ def scrape_systems(url=r"https://www.ana.gov.br"):
     url: string. The URL which contains all systems.
 
     Return:
-    systems: list of named tuples holding the systems and its URLs.
+    systems: list of named tuples holding the systems names, ids and its URLs.
     """
     page = requests.get(url + r'/sar0/Home', verify=False)
     soup = BeautifulSoup(page.text, 'html.parser')
     elements = soup.find_all(string="Dados Históricos")
 
-    systems = list()
-    SystemNamedTuple = namedtuple("system", ["name", "url"])
+    sys_id = list(range(len(elements)))
+    urls = []
+    name = []
+
     for elm in elements:
-        sys_url = url + elm.parent.parent.parent.find('a', href=True)['href']
-        name = elm.parent.parent.parent.parent.parent.find_next('strong').text
-        systems.append(SystemNamedTuple(name, sys_url))
-    return systems
+        urls.append(elm.parent.parent.parent.find('a', href=True)['href'])
+        name.append(elm.parent.parent.parent.parent.parent.find_next('strong'))
+
+    urls = [url + u for u in urls]
+    name = [n.text for n in name]
+    df_sys = pd.DataFrame({"system_id": sys_id,
+                           "system_name": name,
+                           "system_url": urls})
+    return df_sys
 
 ##############################################################################
 
-def scrape_reservoirs(all_systems):
-    """Scrape the names and codes of all reservoirs, of all systems.
+def scrape_reservoirs(df_sys):
+    """Scrape the names and the codes of all reservoirs, of all systems.
 
     Args:
-    all_systems: list of named tuples holding the all systems names and its 
-    URLs.
+    df_sys: Pandas DataFrame holding the systems ids, names and URLs.
 
     Returns:
-    df_res: Pandas DataFrame holding reservoirs codes and names of all systems.
+    df_res: Pandas DataFrame holding reservoirs' codes, names, URLs, and its
+    corresponding system id and system name.
     """
-    temp = [None] * len(all_systems) # List of all all_systems data frames
-    for i, sys in enumerate(all_systems):
-        page = requests.get(sys.url, verify=False)
+    now = datetime.datetime.now()
+    temp = [] # List of all all_systems data frames
+
+    for sys_id, sys_url in zip(df_sys["system_id"], df_sys["system_url"]):
+        page = requests.get(sys_url, verify=False)
         soup = BeautifulSoup(page.text, 'html.parser')
         element = soup.find('select',
                             class_="form-control input-m-sm",
-                            id="dropDownListReservatorios")       
+                            id="dropDownListReservatorios")
         reservoirs = element.find_all_next("option")[1:]
         names = [None] * len(reservoirs)
         codes = [None] * len(reservoirs)
+
         for j, res in enumerate(reservoirs):
             names[j] = " ".join(res.text.split())
-            codes[j] = int(res['value'])
+            codes[j] = res['value']
 
-        df_res = pd.DataFrame({"res_code": codes,
-                               "res_name": names,
-                               "system_id": [i] * len(reservoirs)})
-        temp[i] = df_res
-    return pd.concat(temp, ignore_index=True)
+        df_tmp = pd.DataFrame({"reservoir_code": codes,
+                               "reservoir_name": names,
+                               "system_id": [sys_id] * len(reservoirs)})
+        temp.append(df_tmp)
 
-##############################################################################
-
-def get_url(sys_url, reservoir_code, day, month, year):
-    """"Return the URL of a given reservoir, of a given system.
-
-    Args:
-    sys_url: String. The URL of the system.
-    reservoir_code: Integer. The code of the reservoir.
-    day, month, year: String for day, month and year respectively.
-
-    Returns:
-    url: String. The reservoir URL."""
-
-    reservoir = r"?dropDownListReservatorios=" + str(reservoir_code)
-    initial = r"&dataInicial=22%2F04%2F1500"
-    final = r"&dataFinal={}%2F{}%2F{}".format(day, month, year)
-    url = sys_url + reservoir + initial + final
-    return url
+    df_res = pd.concat(temp, ignore_index=True)
+    df_res = df_res.merge(df_sys, how='left', on="system_id")
+    df_res.rename(columns={"system_url": "reservoir_url"}, inplace=True)
+    # Assembly the reservoir URL:
+    df_res["reservoir_url"] = df_res["reservoir_url"] + \
+                              r"?dropDownListReservatorios=" + \
+                              df_res["reservoir_code"] + \
+                              r"&dataInicial=22%2F04%2F1500" + \
+                              r"&dataFinal=" + now.strftime("%d") + \
+                              r"%2F" + now.strftime("%m") + \
+                              r"%2F" + now.strftime("%Y") + \
+                              r"&button=Buscar"
+    # Reorder columns:
+    df_res = df_res[["reservoir_code", "reservoir_name", "reservoir_url",
+                     "system_id", "system_name"]]
+    return df_res
 
 ##############################################################################
 
-def scrape_history(sys_url, struct, reservoir_codes):
-    """"Scrape all available data of all reservoirs of one system.
-
+def get_file_name(sys_id, res_code):
+    """Return the file name of a reservoir.
     Args:
-    struct: list of tuples (constant) holding the relationship between the
-    variables and the text to lookup within the page.
-
-    reservoir_codes: List of all system's reservoir codes.
+    sys_id: Integer. The system id.
+    res_code: The reservoir code.
 
     Returns:
-    df: Pandas Dataframe holding all data of all reservoirs of ONE system.
+    file_name: String with the file name.
     """
+    file_name = "{:02d}_{}.html".format(sys_id, res_code)
+    return file_name
 
-    today = datetime.datetime.now()
-    day = today.strftime("%d")
-    month = today.strftime("%m")
-    year = today.strftime("%Y")
+##############################################################################
 
-    dataframes = [None] * len(reservoir_codes)
-    for i, code in enumerate(reservoir_codes):
-        print(">>> Downloading:", code)
-        url = get_url(sys_url, code, day, month, year)
-        page = requests.get(url, verify=False)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        elements = soup.find('tbody', class_="list")
+def download_page(page_url, file_name):
+    """Download one web page and store it in one file.
+    Args:
+    page_url: String. The URL of the web page to be downloaded.
+    file_name: String. The file to store the web page.
 
-        df_temp = pd.DataFrame()
-        for var_col_map in struct:
-            var = var_col_map[0]
-            text = var_col_map[1]
-            temp = elements.find_all('td', class_=text)
-            temp = [v.text for v in temp]
-            df_temp[var] = temp
-        df_temp["reservoir_code"] = [code] * len(temp)
-        dataframes[i] = df_temp
+    Returns:
+    response.status_code: Integer. Status of requests library, 200 is success.
+    """
+    response = requests.get(page_url, verify=False)
+    if response.status_code == 200:
+        with open(file_name, mode='wb') as file:
+            file.write(response.content)
+    print(">>> File: {} - Status: {}.".format(file_name, response.status_code))
+    return response.status_code
 
-    df_sys = pd.concat(dataframes)
-    for var_col_map in struct[:-1]:
-        var = var_col_map[0]
-        df_sys[var] = df_sys[var].str.replace(',', '.').astype('float')
-    df_sys["date"] = pd.to_datetime(df_sys["date"], dayfirst=True)
-    return df_sys
+##############################################################################
+
+def download_all_history(df_res):
+    """"Download all reservoir's home pages. Each reservoir home page is stored
+    in one single .html file using the following name pattern:
+    "SYSTEM_ID" + "_" + "RESERVOIR_CODE" + ".html"
+
+    Args:
+    df_res: Pandas DataFrame holding reservoirs' codes, names, URLs, and its
+    corresponding system id and system name.
+
+    Returns:
+    df: Pandas DataFrame holding reservoirs' codes, names, URLs, and its
+    corresponding system id and system name, the corresponding .html file name
+    and the status of the page request.
+    """
+    df_tmp = df_res.copy()
+    df_tmp["file"] = df_tmp.apply(lambda x: get_file_name(x["system_id"],
+                                                          x["reservoir_code"]),
+                                  axis=1)
+    df_tmp["file"] = df_tmp["file"].apply(lambda x:
+                                          os.path.join(RESERVOIR_DIR, x))
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.map(download_page,
+                              df_tmp["reservoir_url"],
+                              df_tmp["file"])
+
+    df_tmp["status"] = list(future)
+    return df_tmp
 
 if __name__ == '__main__':
 
-    systems = scrape_systems()        
-    df_reservoirs = scrape_reservoirs(systems)
+    df_systems = scrape_systems()
+    df_reservoirs = scrape_reservoirs(df_systems)
 
-    if not os.path.isdir(DATA_DIR):
-        os.mkdir(DATA_DIR)
+    df_reservoirs = download_all_history(df_reservoirs)
 
-    # Scrape all data, of all reservoirs of 'Nordeste e Semiárido' system:
-    res_codes = df_reservoirs.loc[df_reservoirs.system_id == 0,
-                                  "res_code"].to_list()
-    df_nordeste = scrape_history(systems[0].url,
-                                 NORDESTE,
-                                 res_codes)
+    if not os.path.isdir(RESERVOIR_DIR):
+        os.makedirs(RESERVOIR_DIR)
 
-    # Scrape all data, of all reservoirs of 'SIN' system:
-    res_codes = df_reservoirs.loc[df_reservoirs.system_id == 1,
-                                  "res_code"].to_list()
-    df_sin = scrape_history(systems[1].url,
-                            SIN,
-                            res_codes)
-
-    # Scrape all data, of all reservoirs of 'Cantareira' system:
-    res_codes = df_reservoirs.loc[df_reservoirs.system_id == 2,
-                                  "res_code"].to_list()
-    df_cantareira = scrape_history(systems[2].url,
-                                   CANTAREIRA,
-                                   res_codes)
-    os.chdir(DATA_DIR)
-    df_cantareira.to_excel("cantareira.xlsx", index=False)
-
-
+    df_systems.to_excel(os.path.join(DATA_DIR, "systems.xlsx"),
+                        index=False)
+    df_reservoirs.to_excel(os.path.join(DATA_DIR, "reservoirs.xlsx"),
+                           index=False)
